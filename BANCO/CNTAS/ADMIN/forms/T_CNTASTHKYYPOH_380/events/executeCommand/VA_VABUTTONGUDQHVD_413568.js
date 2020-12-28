e762a1cd0044929a181fbb9b059bf13477e95825
Dario Herrera <dario97herrera@gmail.com>
2020-12-28 13:23:33 -0500

    Version antes de almuerzo

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASTHKYYPOH_380/events/executeCommand/VA_VABUTTONGUDQHVD_413568.js
@@ -0,0 +1,11 @@
+
+
+//Entity: FiltroCedula
+//FiltroCedula. (Button) View: frmCrearCuenta
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONGUDQHVD_413568 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.FiltroCedula = true;
+
+};
--- BANCO/CNTAS/ADMIN/forms/T_CNTASTHKYYPOH_380/events/executeQuery/Q_CLIETNEE_OB83.js
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASTHKYYPOH_380/events/executeQuery/Q_CLIETNEE_OB83.js
@@ -1,6 +1,8 @@
 
 //ClienteQuery Entity: Cliente
 task.executeQuery.Q_CLIETNEE_OB83 = function(executeQueryEventArgs){
+    executeQueryEventArgs.parameters.cedula = executeQueryEventArgs.commons.api.vc.model.FiltroCedula.cedula;
     executeQueryEventArgs.commons.execServer = true;
+    
     //executeQueryEventArgs.commons.serverParameters.Cliente = true;
 };
